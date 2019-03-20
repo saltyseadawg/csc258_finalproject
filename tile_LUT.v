35c2e6b749ab50058cbc6edd641073170dbea79a
Sorano <sabrinayu7@gmail.com>
2019-03-20 13:54:42 -0400

    expanded to deal with boot scenario and random sequence generation

--- tile_LUT.v
+++ tile_LUT.v
@@ -1,10 +1,12 @@
-module tile_LUT(in, x, y, colour);
-	input [1:0];
+module tile_LUT(in, x, y, colour, load_random);
+	input [1:0] in;
 	output reg [2:0] colour;
 	output reg [7:0] x;
 	output reg [6:0] y;
+	input load_random;
 	 
 	always @(*)
+		if (load_random) begin
 		case(in):
 			2'b00: begin
 				x = 8'd0;
@@ -19,17 +21,19 @@ module tile_LUT(in, x, y, colour);
 			2'b10: begin
 				x = 8'd0;
 				y = 7'd8;
-				colour = 3'b100;
+				colour = 3'b011;
 			end
 			2'b11: begin
 				x = 8'd8;
 				y = 7'd8;
-				colour = 3'b011;
+				colour = 3'b100;
 			end
 			default: begin
 				x = 8'd0;
 				y = 7'd0;
 				colour = 3'b000;
 			end
+		endcase
+		end
 			
 endmodule 
