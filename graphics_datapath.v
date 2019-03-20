a656ebdb7c07a85849d11de82a38b5123c9ed20b
Sorano <sabrinayu7@gmail.com>
2019-03-20 19:28:56 -0400

    removed data values for colour and coordinates

--- graphics_datapath.v
+++ graphics_datapath.v
@@ -1,9 +1,11 @@
 //data path for graphics
 //figure out how to write to larger part of screen - lab moniter is 1680 x 1050
 
-module graphics_datapath(clock, ,x_out, y_out, load, enable, resetn);
+module graphics_datapath(clock, x_out, y_out, load, enable, resetn, x_in, y_in, flash, colour_in, colour_out);
 
-	input clock, load, enable, resetn;
+	input clock, load, enable, resetn, flash;
+	input [7:0] x_in, y_in;
+	input [2:0] colour_in;
 	
 	output [7:0] x_out;
 	output [6:0] y_out;
@@ -11,6 +13,7 @@ module graphics_datapath(clock, ,x_out, y_out, load, enable, resetn);
 	
 	reg [7:0] x;
 	reg [6:0] y;
+	reg [2:0] colour;
 	
 	//counter for implementing coordinates of pixels
 	reg [5:0] counter;
@@ -19,25 +22,13 @@ module graphics_datapath(clock, ,x_out, y_out, load, enable, resetn);
 		if (!resetn) begin
 			x <= 8'b0;
 			y <= 7'b0;
-			colour <= 3'b0;
+			colour <= 3'b001;
 		end
 		else begin
-			if (load_t0)
-				x <= 8'd0;
-				y <= 7'd0;
-				colour <= 3'b001;
-			if (load_t1)
-				x <= 8'd8;
-				y <= 7'd0;
-				colour <= 3'b010;
-			if (load_t2)
-				x <= 8'd0;
-				y <= 7'd8;
-				colour <= 3'b100;
-			if (load_t3)
-				x <= 8'd8;
-				y <= 7'd8;
-				colour <= 3'b011;
+			if (load)
+				x <= x_in;
+				y <= y_in;
+				colour <= colour_in;
 			if (flash)
 				colour <= 3'b111;
 		end
@@ -47,15 +38,18 @@ module graphics_datapath(clock, ,x_out, y_out, load, enable, resetn);
 	always @(posedge clock) begin
 		if(!resetn)
 			counter <= 6'b000000;
-		else if (enable) begin
-				if (load)
-					counter <= 6'b000000;
-				else
-					counter <= counter + 1'b1;
+		else if (enable)
+			if (load) begin
+				counter <= 6'b000000;
+			end
+			else begin
+				counter <= counter + 1'b1;
 			end
 	end
+			
 	
-	assign x_out = x_in[5:0] + counter[5:3];
-	assign y_out = y_in[5:0] + counter[2:0];
+	assign x_out = x + counter[5:3];
+	assign y_out = y + counter[2:0];
+	assign colour_out = colour;
 	
 endmodule
